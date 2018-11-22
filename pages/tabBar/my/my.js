7d8e74d39d5b3315c0d4b6844eb368bf92eb4135
lx19950727 <945436771@qq.com>
2018-11-22 16:12:17 +0800

    111

--- pages/tabBar/my/my.js
+++ pages/tabBar/my/my.js
@@ -91,7 +91,12 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    wx.showNavigationBarLoading();
+    console.log(11);
+    setTimeout(function(){
+      wx.hideNavigationBarLoading();
+      wx.stopPullDownRefresh();
+    }, 5000);
   },
 
   /**
